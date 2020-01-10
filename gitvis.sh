2e240d861d996bac0b0a1f46b34e46e2f35ef008
smartmic <dev@famic.de>
2020-01-11 00:09:39 +0100

    docs: Correct license boilerplate text

--- gitvis.sh
+++ gitvis.sh
@@ -9,18 +9,18 @@
 #
 # This file is part of git2pic.
 #
-# Foobar is free software: you can redistribute it and/or modify
+# git2pic is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# Foobar is distributed in the hope that it will be useful,
+# git2pic is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with git2pic.  If not, see <http://www.gnu.org/licenses/>.
 
 
 echo "@startuml"
